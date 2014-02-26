850c6b55d1be6872d6ee16cbb38b075d5ca55519
Narendra Singh <narensgh@gmail.com>
2014-02-26 18:43:51 +0530

    Updated application.config.php to load 'DoctrineModule', 'DoctrineORMModule', 'Api', 'Front' modules.

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,11 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',
+         'Application',
+    	'DoctrineModule',
+    	'DoctrineORMModule',
+    	'Api',
+    	'Front'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
