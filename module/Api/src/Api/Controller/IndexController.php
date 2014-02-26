37b5584525b14ad075e85ff10977f8fc1ee2f474
Narendra Singh <narensgh@gmail.com>
2014-02-26 15:58:37 +0530

    Created modules api and front and also changed their references.

---
+++ module/Api/src/Api/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Api\Controller;
+
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
+
+class IndexController extends AbstractRestfulController
+{
+    public function indexAction()
+    {
+        return new JsonModel(array('name'=>"narendra"));
+    }
+}
---
+++ module/Front/src/Front/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Front\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
